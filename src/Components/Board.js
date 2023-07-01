bd39753594335e6868b54876f16b0f1e575809ce
Dev Bachani <dev22603@gmail.com>
2023-07-01 23:37:03 +0530

    Created UI of a Mayfair card of monopoly

---
+++ src/Components/Board.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Board.css';
+// import Card from '../UI/Card';
+import DarkBlue from './DarkBlue';
+
+
+const Board = () =>{
+  return(
+    <div className="board">
+        <DarkBlue>aaaaaaaaaaaaaaaaaaaaaaaa</DarkBlue>
+    </div>
+
+  ) 
+}
+
+export default Board;
---
+++ src/Components/DarkBlue.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../UI/Card'
+import './DarkBlue.css'
+
+
+const DarkBlue = (props) =>{
+  return(
+    <Card className="top property">
+        <div className="DarkBlue"></div>
+        
+    </Card>
+
+  ) 
+}
+
+export default DarkBlue;
